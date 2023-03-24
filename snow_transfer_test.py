3f705430aa89fbeb3049e4c66d0b0fa5b1bbbf41
Zichong Li <zic@amazon.com>
2023-03-23 17:47:22 -0700

    Bug fix for path manipulation; Add unit test

--- snow_transfer_test.py
+++ snow_transfer_test.py
@@ -14,3 +14,14 @@ def test_logfile_generation():
     mod.log_dir = '/tmp'
     mod.create_logger()
     assert os.path.isfile(mod.log_dir + '/snowTransfer-full-%s.log' % mod.current_time)
+
+def test_conv_obj_name():
+    file_name = "/Users/zic/Documents/zictest/testlarge/Olaf.md"
+    obj_name = mod.conv_obj_name(file_name, "", "/Users/zic/Documents/zictest/")
+    assert obj_name == "testlarge/Olaf.md"
+    obj_name = mod.conv_obj_name(file_name, "", "/Users/zic/Documents/zictest")
+    assert obj_name == "testlarge/Olaf.md"
+    obj_name = mod.conv_obj_name(file_name, "test/", "/Users/zic/Documents/zictest")
+    assert obj_name == "test/testlarge/Olaf.md"
+    obj_name = mod.conv_obj_name(file_name, "test", "/Users/zic/Documents/zictest")
+    assert obj_name == "test/testlarge/Olaf.md"
